1a0096006b2886c465a0f43fe0f9e77904ac8d3b
emirojaseng <emirojaseng@gmail.com>
2017-11-12 22:04:58 -0600

    Create mPMedio.m

---
+++ mPMedio.m
@@ -0,0 +1,17 @@
+function [Tn,Wn] = mPMedio(f,y0,I,n)
+%mEuler Método de Euler explícito
+%   resuelve el PVI y'=f(t,y), y(t0)=y0
+% donde el vector I=[t0,t0+T] y n es el numero de pasos
+
+Tn=linspace(I(1),I(2),n); %la malla del tiempo
+Wn=zeros(1,size(Tn,2)); %preset Wn
+Wn(1)=y0; %Condicion inicial
+for i=1:size(Tn,2)-1
+    Wn(i+1)=paso_mPMedio(f,Wn(i), Tn(i), Tn(i+1));
+end
+end
+
+function [w_nuevo]= paso_mPMedio(f,w_viejo, t_viejo,t_nuevo)
+h=t_nuevo-t_viejo;
+w_nuevo=w_viejo+h*f(t_viejo+h/2,w_viejo+h/2*f(t_viejo,w_viejo));
+end
