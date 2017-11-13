eaa670c28cc105e3c8142aa1a53b595c5618ed62
emirojaseng <emirojaseng@gmail.com>
2017-11-12 23:13:18 -0600

    Update mRK4.m

--- mRK4.m
+++ mRK4.m
@@ -13,10 +13,10 @@ end
 
 function [w_nuevo]= paso_RK4(f,w_viejo, t_viejo,t_nuevo)
 h=t_nuevo-t_viejo;
-S=zeros(1,4) %vector donde guardo las aproximaciones que usa el metodo
+S=zeros(1,4); %vector donde guardo las aproximaciones que usa el metodo
   S(1)=f(t_viejo, w_viejo); %pendiente izquierda
   S(2)=f(t_viejo+h/2,w_viejo+h/2*S(1)); %pendiente punto medio
   S(3)=f(t_viejo+h/2,w_viejo+h/2*S(2)); %mejor aproximacion de la pendiente en el punto medio
   S(4)=f(t_viejo+h, w_viejo+h*S(3)); %pendiente mejorada a la derecha
-w_nuevo=w_viejo+h/6*(S(1)+2*S(2)+2*S(3)+S(4)); *El paso
+w_nuevo=w_viejo+h/6*(S(1)+2*S(2)+2*S(3)+S(4)); %El paso
 end
